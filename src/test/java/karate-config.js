13b32c2303fd1723e711398daa10032e5933ec8d
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2023-01-21 23:30:06 -0500

    refactor: remove unused code

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,24 +1,19 @@
 function fn() {
+  // get java env API_KEY
   var apiKeyValue = java.lang.System.getenv("API_KEY");
 
-  var env = karate.env; // get java system property 'karate.env'
+  // get java system property 'karate.env'
+  var env = karate.env;
   karate.log('karate.env system property was:', env);
-  if (!env) {
-    env = 'dev'; // a custom 'intelligent' default
-  }
-  var config = { // base config JSON
+
+  // base config JSON
+  var config = {
     urlBase: 'http://api.countrylayer.com/v2',
     pathAll: 'all',
     pathAlpha: 'alpha',
     pathName: 'name',
     apiKeyValue: apiKeyValue,
   };
-  if (env == 'stage') {
-    // over-ride only those that need to be
-    config.someUrlBase = 'https://stage-host/v1/auth';
-  } else if (env == 'e2e') {
-    config.someUrlBase = 'https://e2e-host/v1/auth';
-  }
 
   // don't waste time waiting for a connection or if servers don't respond within 5 seconds
   karate.configure('connectTimeout', 5000);
